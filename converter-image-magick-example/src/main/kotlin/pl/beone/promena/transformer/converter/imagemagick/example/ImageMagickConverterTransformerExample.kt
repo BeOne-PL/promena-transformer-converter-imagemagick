550e17d746fef80284f7624bffd8742c04da7c12
Bartosz Skotarek <bzskotarek@gmail.com>
2019-09-25 13:58:45 +0200

    [improved] Adapted to new Promena plugin requirements

--- converter-image-magick-example/src/main/kotlin/pl/beone/promena/transformer/converter/imagemagick/example/ImageMagickConverterTransformerExample.kt
+++ converter-image-magick-example/src/main/kotlin/pl/beone/promena/transformer/converter/imagemagick/example/ImageMagickConverterTransformerExample.kt
@@ -5,19 +5,13 @@ import pl.beone.promena.transformer.contract.transformation.Transformation
 import pl.beone.promena.transformer.converter.imagemagick.applicationmodel.imageMagickConverterParameters
 import pl.beone.promena.transformer.converter.imagemagick.applicationmodel.imageMagickConverterTransformation
 
-fun transform(): Transformation {
-    // HTTP: localhost:8080
-    // Repeat: 1
-    // Concurrency: 1
+fun promena(): Transformation {
     // Data: normal.jpeg
 
     return imageMagickConverterTransformation(IMAGE_PNG, imageMagickConverterParameters())
 }
 
-fun `transform _ scale irregularly`(): Transformation {
-    // HTTP: localhost:8080
-    // Repeat: 1
-    // Concurrency: 1
+fun `promena _ scale irregularly`(): Transformation {
     // Data: normal.jpeg
 
     return imageMagickConverterTransformation(IMAGE_PNG, imageMagickConverterParameters(width = 50, height = 100, ignoreAspectRatio = true))
