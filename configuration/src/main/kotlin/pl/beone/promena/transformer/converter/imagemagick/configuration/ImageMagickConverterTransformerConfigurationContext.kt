da7126604952bb1c77aba30454df2ecd80f52270
Bartosz Skotarek <bzskotarek@gmail.com>
2019-10-05 09:26:25 +0200

    [improved] Adapted to new archetype extension functions for configuration context

--- configuration/src/main/kotlin/pl/beone/promena/transformer/converter/imagemagick/configuration/ImageMagickConverterTransformerConfigurationContext.kt
+++ configuration/src/main/kotlin/pl/beone/promena/transformer/converter/imagemagick/configuration/ImageMagickConverterTransformerConfigurationContext.kt
@@ -1,11 +1,11 @@
 package pl.beone.promena.transformer.converter.imagemagick.configuration
 
-import org.joda.time.format.PeriodFormatterBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
 import pl.beone.promena.transformer.converter.imagemagick.ImageMagickConverterTransformerDefaultParameters
-import java.time.Duration
+import pl.beone.promena.transformer.converter.imagemagick.configuration.extension.getNotBlankProperty
+import pl.beone.promena.transformer.converter.imagemagick.configuration.extension.toDuration
 
 @Configuration
 class ImageMagickConverterTransformerConfigurationContext {
@@ -17,24 +17,6 @@ class ImageMagickConverterTransformerConfigurationContext {
     @Bean
     fun imageMagickConverterTransformerDefaultParameters(environment: Environment): ImageMagickConverterTransformerDefaultParameters =
         ImageMagickConverterTransformerDefaultParameters(
-            environment.getProperty("$PROPERTY_PREFIX.default.parameters.timeout").ifSet { it.toDuration() }
-        )
-
-    private fun <T> String?.ifSet(toRun: (String) -> T): T? =
-        when {
-            this == null || isBlank() -> null
-            else -> toRun(this)
-        }
-
-    private fun String.toDuration(): Duration =
-        Duration.ofMillis(
-            PeriodFormatterBuilder()
-                .appendDays().appendSuffix("d")
-                .appendHours().appendSuffix("h")
-                .appendMinutes().appendSuffix("m")
-                .appendSeconds().appendSuffix("s")
-                .appendMillis().appendSuffix("ms")
-                .toFormatter()
-                .parsePeriod(this).toStandardDuration().millis
+            environment.getNotBlankProperty("$PROPERTY_PREFIX.default.parameters.timeout")?.toDuration()
         )
 }
